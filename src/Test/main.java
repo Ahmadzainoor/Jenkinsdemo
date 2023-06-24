0ab9dec3c1920ea1b6f855533125a86e9c49cdde
Ahmadzainoor <=>
2023-06-24 14:19:18 -0400

    first commit

---
+++ src/Test/main.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.print("this is our first Jenkins code");
+      //want to print it 5 times.
+
+        for(int i =0; i <5; i++){
+            System.out.println("this is our first Jenkins code");
+        }
+    }
+}
